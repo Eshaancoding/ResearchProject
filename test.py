1428cab8d0013e41c2015e1b792944fd1ae066de
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2022-10-11 16:09:21 -0400

    VNN Chess Dataset (not working)

--- test.py
+++ test.py
@@ -14,11 +14,11 @@ bias_model = nn.Sequential(
     nn.Linear(12, 1),
 )
 
-vnn_block = VNNBlock(weight_model, bias_model, d_model)
+vnn_block = VNNBlock(d_model, weight_model, bias_model).to("cuda")
 
-input = torch.rand(6, 5)
+input = torch.rand(6, 5).to("cuda")
 
-extra_out = torch.rand(6, 10, 10)
+extra_out = torch.rand(6, 10, 10).to("cuda")
 
 start = time.time()
 print(vnn_block(input, extra_out=extra_out, output_size=10, chunks=1).shape)
