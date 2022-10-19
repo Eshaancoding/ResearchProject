cf61a9db970423388741e20e8e55e8ab62b46711
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2022-10-18 20:06:14 -0400

    Test DQN test (still not working)

--- ChessBenchmark/DQN.py
+++ ChessBenchmark/DQN.py
@@ -10,9 +10,10 @@ from copy import deepcopy
 class ReplayMemory ():
     def __init__(self, max_len) -> None:
         self.max_len = max_len 
+
         self.states = []
-        self.rewards = []
         self.next_states = []
+        self.rewards = []
         self.dones = []
         self.actions = []
 
@@ -42,22 +43,28 @@ class ReplayMemory ():
         self.next_states = self.next_states[-self.max_len:]
         self.rewards = self.rewards[-self.max_len:]
         self.dones = self.dones[-self.max_len:]
-
+        self.actions = self.actions[-self.max_len:]
+        
     def get_batch (self, batch_size):
-        actions_return = [] 
+        indexes = torch.randperm(len(self.states))[:batch_size].tolist()
+
         states_return = torch.tensor([])
         next_states_return = torch.tensor([])
         rewards_return = torch.tensor([])
         dones_return = torch.tensor([])
+        actions_return = [] 
 
-        for _ in range(batch_size):
-            random_index = randint(0, len(self.states)-1)
+        for i in range(batch_size):
+            if i > len(indexes):
+                random_index = randint(0, len(self.states))
+            else: 
+                random_index = indexes[i] 
             
-            actions_return.append(self.actions[random_index]) 
             states_return = self.addToTensor(states_return, self.states[random_index])
             next_states_return = self.addToTensor(next_states_return, self.next_states[random_index])
             rewards_return = self.addToTensor(rewards_return, self.rewards[random_index])
             dones_return = self.addToTensor(dones_return, self.dones[random_index])
+            actions_return.append(self.actions[random_index]) 
     
         return actions_return, states_return, next_states_return, rewards_return, dones_return
 
@@ -70,9 +77,9 @@ class DQN:
         gamma,                          # Gamma Hyperparameter (used for balancing importance of future rewards) 
         lr,                             # Learning rate hyperparameter used during training
         model_path,                     # Model path for saving the model (could be None to disable saving the model) 
-        log_per_epi,                    # Save the model per # episodes (could be None to disable saving the model)
+        should_load_from_path,          # If it should load the model from the specified model_path argument
+        save_per_epi,                   # Save the model per # episodes (could be None to disable saving the model)
         update_target_model_per_epi,    # Update target weights with the original model weights per # epsidoes 
-        max_test_itr                    # Max amount of iterations in an episode when testing the model
     ) -> None:
 
         self.model = model # The model will act as a value function but will output the value of each output in the output layer of the neural network, thus acting as a q-value function
@@ -82,17 +89,16 @@ class DQN:
         self.gamma = gamma
         self.opt = torch.optim.Adam(self.model.parameters(), lr=lr)
         self.mse_loss = torch.nn.MSELoss()
-        self.max_test_itr = max_test_itr
 
         # Device
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
         # Model saving
         self.model_path = model_path
-        self.log_per_epi = log_per_epi
+        self.save_per_epi = save_per_epi
 
         # Check if model path exists, and if so load the model
-        if self.model_path != None and self.log_per_epi != None and exists(model_path):
+        if self.model_path != None and should_load_from_path and exists(model_path):
             print(f"Loading model from path: {model_path}")
             self.model = torch.load(model_path, map_location=torch.device(self.device))
 
@@ -104,23 +110,18 @@ class DQN:
         self.target_model.to(self.device)
         self.model.to(self.device)
 
-    def test (self, env, max_num_itr, render):
+    def test (self, env, render):
         avg_reward = 0
-        
-        # ========================== DELETE THIS LATER ===================================    
-        state, _ = env.reset()
-        # ========================== DELETE THIS LATER ===================================    
-
-        state = [state for i in range(2)]
+        done = False 
+        state = env.reset()
         itr = 0
 
-        for _ in range(max_num_itr):
+        while not done:
             x = torch.tensor(state).to(torch.float).to(self.device)
             move = self.model(x)
             state, reward, done, _ = env.step(torch.argmax(move, dim=0).item())
             avg_reward += reward
             itr += 1
-            if done: break
             if render:
                 env.render()
 
@@ -133,14 +134,6 @@ class DQN:
         avg_reward = 0
 
         for episode in progress_bar:
-            state, _ = env.reset()
-
-            # ========================== DELETE THIS LATER ===================================    
-            state = [state for i in range(2)]
-            # ========================== DELETE THIS LATER ===================================    
-
-            done = False
-
             if use_database: database_rand = randint(0, 2)
             else: database_rand = 1 
 
@@ -148,6 +141,8 @@ class DQN:
                 # 1/3 of the chance to draw from master database
                 self.replay_mem.add(*env.get_database())
             else:
+                state = env.reset()
+                done = False
                 # Go to the actual environment
                 while not done: 
                     rand_num = randint(0, 100) 
@@ -164,34 +159,39 @@ class DQN:
 
             # Sample random minibatch of transitions from D, and get expected y value
             action_batch, state_batch, next_state_batch, rewards_batch, done_batch = self.replay_mem.get_batch(self.batch_size)
-            y = rewards_batch + (self.gamma * torch.max(self.target_model(next_state_batch), dim=1).values * done_batch)
-
+            y = rewards_batch + (self.gamma * torch.max(self.target_model(next_state_batch), dim=1).values * (1 - done_batch))
+            
             # Train on y value as target
             self.opt.zero_grad() 
             out = self.model(state_batch)
             
             # Set the y value that is corresponding with the target
             # If there is anyway to do this without a for loop, let me know. 
-            target = out.detach()
+            target = deepcopy(out.detach())
             for i in range(self.batch_size):
                 target[i][action_batch[i]] = y[i]
             
             loss = self.mse_loss(out, target)
             loss.backward()
+            if loss.item() > 10:
+                raise Exception("SDFSDFSDF")
             self.opt.step()
-
             # log progress bar
             if use_tqdm: 
-                progress_bar.set_description(f"Episode: {episode} Avg reward: {avg_reward:.2f} Last Model Saved: {last_saved_itr}")
+                progress_bar.set_description(f"Episode: {episode} Avg reward: {avg_reward:.2f} loss: {loss.item():.3f} model saved: {last_saved_itr}")
 
             # Save model
-            if self.model_path != None and self.log_per_epi != None and episode != 0 and episode % self.log_per_epi == 0: 
+            if self.model_path != None and self.save_per_epi != None and episode != 0 and episode % self.save_per_epi == 0: 
                 torch.save(self.model, self.model_path) 
-                last_saved_itr = f"On Episode: {episode}"
+                last_saved_itr = f"epi {episode}"
 
             # Update target model
             if episode != 0 and episode % self.update_target_model_per_epi == 0:
                 self.target_model.load_state_dict(self.model.state_dict())
 
                 # Test model
-                avg_reward = self.test(env=env, max_num_itr=self.max_test_itr, render=render) 
+                avg_reward = self.test(env=env, render=render) 
+
+        
+        if self.model_path != None and self.save_per_epi != None and episode != 0 and episode % self.save_per_epi == 0: 
+            torch.save(self.model, self.model_path) 
--- ChessBenchmark/testDQN.py
+++ ChessBenchmark/testDQN.py
@@ -1,38 +1,39 @@
 import gym
 from DQN import *
 from torch import nn
-import torch
+test = False
 
 # Declare model
 model = nn.Sequential(
-    nn.Linear(2, 100),
+    nn.Linear(4, 100),
     nn.ReLU(),
-    nn.Linear(100, 3),
+    nn.Linear(100, 2),
 )
 
 # Declare trainer
 trainer = DQN(
     model=model,
-    replay_mem_max_len=500,
-    epsilon=0.1,
-    batch_size=64,
-    gamma=0.99,
+    replay_mem_max_len=1_000,
+    epsilon=0.5,
+    batch_size=16,
+    gamma=0.98,
     lr=0.01,
-    model_path="MountainCarDQNTest.pth",
-    log_per_epi=500,
+    model_path="CartpoleV1.pth",
+    save_per_epi=100,
     update_target_model_per_epi=15,
-    max_test_itr=100
 )
 
 # Make environment
-env = gym.make('MountainCar-v0')
+env = gym.make('CartPole-v1')
 
-# Train model
-#trainer.train(
-        #    env=env,
-        #num_episodes=5_000,
-        #use_database=False, # Only if we are using chess database
-        #use_tqdm=True,
-        #render=False
-        #)
-trainer.test(env, 10000, render=True)
+# Train or test model
+if test:
+    trainer.test(env, 10000, render=True)
+else:
+    trainer.train(
+        env=env,
+        num_episodes=5_000,
+        use_database=False, # Only if we are using chess database
+        use_tqdm=True,
+        render=False
+    )
