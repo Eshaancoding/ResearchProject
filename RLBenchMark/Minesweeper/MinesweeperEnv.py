ffab98e70da540f0f9390991ba7f393cb2e1b289
Eshaan Barkataki <eshaanbarkataki@gmail.com>
2022-11-09 13:09:50 -0500

    minesweeper env works!!

--- RLBenchMark/Minesweeper/MinesweeperEnv.py
+++ RLBenchMark/Minesweeper/MinesweeperEnv.py
@@ -1,19 +1,59 @@
 from minesweeper.msgame import MSGame
+import random
 
 class MinesweeperEnv:
     def get_possible_moves (self):
-        print(self.game.get_info_map())
-        return None
+        info_map = self.game.get_info_map()
+        possible_moves = []
+        for x in range(self.board_size):
+            for y in range(self.board_size):
+                if info_map[x][y] == 11:
+                    possible_moves.append((x, y))
+        
+        return possible_moves
 
-    def reset (self):
-        self.game = MSGame(10, 10, 5)
-        self.possible_moves = self.get_possible_moves()
+    def random_move (self):
+        index = random.randint(0, len(self.possible_moves)-1)
+        return self.possible_moves[index]
+
+    def reset (self, board_size=10, num_mines=20):
+        self.board_size = board_size
+        while True:
+            self.game = MSGame(board_size, board_size, num_mines)
+            self.possible_moves = self.get_possible_moves()
+
+            x, y = self.random_move()
+            self.game.play_move("click", x, y)
+            self.possible_moves = self.get_possible_moves()
+            
+            if self.game.game_status == 2: break
+
+        return self.game.get_info_map(), self.possible_moves
 
     def step (self, index):
+        reward = 1
+        x, y = self.possible_moves[index]
+        self.game.play_move("click", x, y)
         
+        is_done = False
+        if self.game.game_status == 1: 
+            # The AI WON!!!
+            is_done = True
+            reward = 10
+        elif self.game.game_status == 0:
+            # The AI LOST!!
+            is_done = False
+            reward = -10
+        
+        self.possible_moves = self.get_possible_moves()
+        return self.game.get_info_map(), self.possible_moves, reward, is_done, None, None
+
+    def manual_step (self, x, y):
+        self.game.play_move("click", x, y)
 
     def render (self):
         self.game.print_board()
 
 env = MinesweeperEnv()
-env.get_possible_moves()
+state, extra_state = env.reset()
+print(state, len(extra_state))
--- RLBenchMark/RL/DQN.py
+++ RLBenchMark/RL/DQN.py
@@ -1,10 +1,12 @@
+from re import S
+from pyparsing import original_text_for
 import torch
 from random import randint
 from tqdm import trange
-from Chess.ChessEnv import * 
+from ChessEnv import * 
 from os.path import exists
 from copy import deepcopy
-from RL.ReplayMemory import *
+from ReplayMemory import *
 import time
 import gym
 
@@ -20,15 +22,13 @@ class DQN:
         should_load_from_path,          # If it should load the model from the specified model_path argument
         save_per_epi,                   # Save the model per # episodes (could be None to disable saving the model)
         update_target_model_per_epi,    # Update target weights with the original model weights per # epsidoes 
-        epsilon_decay,              
+        epsilon_decay,
         epsilon_min,
         test_per_epi
     ) -> None:
-        # Device
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
         self.model = model # The model will act as a value function but will output the value of each output in the output layer of the neural network, thus acting as a q-value function
-        self.replay_mem = ReplayMemory(max_len=replay_mem_max_len, device=self.device)
+        self.replay_mem = ReplayMemory(max_len=replay_mem_max_len)
         self.epsilon = epsilon
         self.batch_size = batch_size
         self.gamma = gamma
@@ -37,6 +37,10 @@ class DQN:
         self.epsilon_decay = epsilon_decay
         self.epsilon_min = epsilon_min
         self.test_per_epi = test_per_epi
+
+        # Device
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = "cpu"
 
         # Model saving
         self.model_path = model_path
@@ -55,26 +59,34 @@ class DQN:
         self.target_model.to(self.device)
         self.model.to(self.device)
 
-    def test (self, env):
-        test_env = deepcopy(env)
+    def test (self, env, render):
+        if render: 
+            test_env = gym.make(env.unwrapped.spec.id, render_mode="human")
+        else:
+            test_env = gym.make(env.unwrapped.spec.id)
+
         avg_reward = 0
         done = False 
-        state, extra_state, _ = test_env.reset()
+        state, _ = test_env.reset()
+        state = list(state)
         itr = 0
 
         while not done:
-            x = state.to(torch.float).to(self.device)
-            extra_state = extra_state.to(torch.float).to(self.device)
-            move = torch.argmax(self.model(x, extra_state), dim=0).item()
+            x = torch.tensor(state).to(torch.float).to(self.device)
+            move = torch.argmax(self.model(x)).item()
 
-            state, extra_state, reward, done, _, _ = test_env.step(move)
+            state, reward, done, _, _ = test_env.step(move)
+            if done: 
+                reward = -10
 
+            state = list(state)
             avg_reward += reward
             itr += 1
+        test_env.close()
 
         return avg_reward / itr
 
-    def train (self, env, num_episodes, use_tqdm=False, should_test=True):
+    def train (self, env, num_episodes, use_tqdm=False, render=False):
         if use_tqdm: progress_bar = trange(num_episodes)
         else: progress_bar = range(num_episodes)
         last_saved_itr = "Not saved" 
@@ -82,8 +94,8 @@ class DQN:
         opt = torch.optim.Adam(self.model.parameters(), lr=self.lr)
 
         for episode in progress_bar:
-            # Test env
-            state, extra_state, _ = env.reset()
+            state, _ = env.reset()
+            state = list(state)
             done = False
             i = 0
             while not done: 
@@ -92,38 +104,34 @@ class DQN:
                     # Do random action
                     move = env.action_space.sample()
                 else:
-                    x = state.to(torch.float).to(self.device)
-                    extra_state = extra_state.to(torch.float).to(self.device)
-                    move = torch.argmax(self.model(x, extra_state)).item()
+                    x = torch.tensor(state).to(torch.float).to(self.device)
+                    move = torch.argmax(self.model(x)).item()
 
                 if self.epsilon > self.epsilon_min: self.epsilon *= self.epsilon_decay
 
-                next_state, next_extra_state, reward, done, _, _ = env.step(move)
-
-                self.replay_mem.add(move, state, next_state, reward, done, extra_state, next_extra_state) 
-                
+                next_state, reward, done, _, _ = env.step(move)
+                if done: reward = -10
+                next_state = list(next_state)
+                self.replay_mem.add(move, state, next_state, reward, done) 
                 state = next_state
-                extra_state = next_extra_state
                 i += 1
 
             # Sample random minibatch of transitions from D, and get expected y value
-            losses = torch.tensor([]).to(self.device)
-            opt.zero_grad() 
-            for state, extra_state, next_state, extra_next_state, action, reward, done in self.replay_mem.get_batch(self.batch_size):
-                y = reward + (self.gamma * torch.max(self.target_model(next_state, extra_next_state), dim=0).values * (1 - done))
+            action_batch, state_batch, next_state_batch, rewards_batch, done_batch = self.replay_mem.get_batch(self.batch_size)
+            y = rewards_batch + (self.gamma * torch.max(self.target_model(next_state_batch), dim=1).values * (1 - done_batch))
             
-                # Set the y value that is corresponding with the target
-                # If there is anyway to do this without a for loop, let me know. 
-                
-                # train 
-                out = self.model(state, extra_state)
-                target = out.detach().clone()
-                target[action] = y
+            # Set the y value that is corresponding with the target
+            # If there is anyway to do this without a for loop, let me know. 
+            
+            # train 
+            opt.zero_grad() 
+            out = self.model(state_batch)
 
-                loss = self.mse_loss(out, target)
-                losses = torch.hstack((losses, loss))
+            target = out.detach().clone()
+            for i in range(self.batch_size):
+                target[i][action_batch[i]] = y[i]
 
-            loss = torch.mean(losses)
+            loss = self.mse_loss(out, target)
             loss.backward()
             opt.step()
 
@@ -142,8 +150,8 @@ class DQN:
                 self.target_model.load_state_dict(self.model.state_dict())
 
             # Test
-            if episode != 0 and episode % self.test_per_epi == 0 and should_test: 
-                avg_reward = self.test(env=env) 
+            if episode != 0 and episode % self.test_per_epi == 0: 
+                avg_reward = self.test(env=env, render=render) 
 
         if self.model_path != None and self.save_per_epi != None and episode != 0 and episode % self.save_per_epi == 0: 
-            torch.save(self.model, self.model_path) 
+            torch.save(self.model, self.model_path)  
